e8e7eb935dd61a2d5b1608c029534443716d3641
Unknown <rorynickolls@gmail.com>
2017-10-03 19:12:26 +0100

    find names from email address
    program will now find an ECS member's name with the HTMLUnit library when given their email address

--- sc01/src/nickolls/rory/sc01/ECSScraper.java
+++ sc01/src/nickolls/rory/sc01/ECSScraper.java
@@ -1,6 +1,43 @@
 package nickolls.rory.sc01;
 
-public class ECSScraper {
-	
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+public class ECSScraper {
+
+	public String findName(String email)
+	{
+		String name = "USER NOT FOUND";
+		
+		String query = email.split("@")[0];
+		
+		String url = "http://www.ecs.soton.ac.uk/people/" + query;
+		
+		WebClient client = new WebClient();
+		client.getOptions().setJavaScriptEnabled(false);
+		client.getOptions().setCssEnabled(false);
+		
+		try
+		{
+			System.out.println("Attempting connection to " + url);
+			HtmlPage page = client.getPage(url);
+			System.out.println("Connection successful.");
+			
+			HtmlElement firstHeader = page.getFirstByXPath("//h1[@class='uos-page-title uos-main-title uos-page-title-compressed']");
+			if(firstHeader != null)
+			{
+				name = firstHeader.asText();
+			}
+		} 
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		client.close();
+		
+		return name;
+		
+	}
 }
--- sc01/src/nickolls/rory/sc01/Main.java
+++ sc01/src/nickolls/rory/sc01/Main.java
@@ -3,6 +3,8 @@ package nickolls.rory.sc01;
 public class Main {
 	public static void main(String[] args)
 	{
-		
+		ECSScraper scraper = new ECSScraper();
+		String name = scraper.findName("ks14g13@ecs.soton.ac.uk");
+		System.out.println(name);
 	}
 }
